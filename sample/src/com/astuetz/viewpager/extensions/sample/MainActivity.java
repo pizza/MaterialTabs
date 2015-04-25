d98d188d25af79f7cdf43d4792717fdcc52604dd
Karim Frenn <karim@oneplus.net>
2015-04-25 12:28:41 +0300

    Added nineoldandroids library for support. Added MaterialRippleLayout code.
    Deleted useless code from the sample.

--- sample/src/com/astuetz/viewpager/extensions/sample/MainActivity.java
+++ sample/src/com/astuetz/viewpager/extensions/sample/MainActivity.java
@@ -16,11 +16,11 @@
 
 package com.astuetz.viewpager.extensions.sample;
 
-import android.graphics.Color;
+import com.astuetz.PagerSlidingTabStrip;
+import com.readystatesoftware.systembartint.SystemBarTintManager;
+
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
-import android.graphics.drawable.LayerDrawable;
-import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -30,12 +30,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.TypedValue;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
-
-import com.astuetz.PagerSlidingTabStrip;
-import com.readystatesoftware.systembartint.SystemBarTintManager;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -50,8 +45,6 @@ public class MainActivity extends ActionBarActivity {
     ViewPager pager;
 
     private MyPagerAdapter adapter;
-    private Drawable oldBackground = null;
-    private int currentColor;
     private SystemBarTintManager mTintManager;
 
     @Override
@@ -60,6 +53,10 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.inject(this);
         setSupportActionBar(toolbar);
+
+        Drawable colorDrawable = new ColorDrawable(getResources().getColor(R.color.green));
+        getSupportActionBar().setBackgroundDrawable(colorDrawable);
+
         // create our manager instance after the content view is set
         mTintManager = new SystemBarTintManager(this);
         // enable status bar tint
@@ -67,11 +64,9 @@ public class MainActivity extends ActionBarActivity {
         adapter = new MyPagerAdapter(getSupportFragmentManager());
         pager.setAdapter(adapter);
         tabs.setViewPager(pager);
-        final int pageMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, getResources()
-                .getDisplayMetrics());
+        final int pageMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, getResources().getDisplayMetrics());
         pager.setPageMargin(pageMargin);
         pager.setCurrentItem(1);
-        changeColor(getResources().getColor(R.color.green));
 
         tabs.setOnTabReselectedListener(new PagerSlidingTabStrip.OnTabReselectedListener() {
             @Override
@@ -87,57 +82,9 @@ public class MainActivity extends ActionBarActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_contact:
-                QuickContactFragment.newInstance().show(getSupportFragmentManager(), "QuickContactFragment");
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void changeColor(int newColor) {
-        tabs.setBackgroundColor(newColor);
-        mTintManager.setTintColor(newColor);
-        // change ActionBar color just if an ActionBar is available
-        Drawable colorDrawable = new ColorDrawable(newColor);
-        Drawable bottomDrawable = new ColorDrawable(getResources().getColor(android.R.color.transparent));
-        LayerDrawable ld = new LayerDrawable(new Drawable[]{colorDrawable, bottomDrawable});
-        if (oldBackground == null) {
-            getSupportActionBar().setBackgroundDrawable(ld);
-        } else {
-            TransitionDrawable td = new TransitionDrawable(new Drawable[]{oldBackground, ld});
-            getSupportActionBar().setBackgroundDrawable(td);
-            td.startTransition(200);
-        }
-
-        oldBackground = ld;
-        currentColor = newColor;
-    }
-
-    public void onColorClicked(View v) {
-        int color = Color.parseColor(v.getTag().toString());
-        changeColor(color);
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putInt("currentColor", currentColor);
-    }
-
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        currentColor = savedInstanceState.getInt("currentColor");
-        changeColor(currentColor);
-    }
-
     public class MyPagerAdapter extends FragmentPagerAdapter {
 
-        private final String[] TITLES = {"Categories", "Home", "Top Paid", "Top Free", "Top Grossing", "Top New Paid",
-                "Top New Free", "Trending"};
+        private final String[] TITLES = {"Categories", "Home"};// "Top Paid", "Top Free", "Top Grossing", "Top New Paid", "Top New Free", "Trending"};
 
         public MyPagerAdapter(FragmentManager fm) {
             super(fm);
--- sample/src/com/astuetz/viewpager/extensions/sample/SuperAwesomeCardFragment.java
+++ sample/src/com/astuetz/viewpager/extensions/sample/SuperAwesomeCardFragment.java
@@ -19,13 +19,9 @@ package com.astuetz.viewpager.extensions.sample;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewCompat;
-import android.util.TypedValue;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
-import android.widget.FrameLayout.LayoutParams;
 import android.widget.TextView;
 
 import butterknife.ButterKnife;
@@ -33,33 +29,33 @@ import butterknife.InjectView;
 
 public class SuperAwesomeCardFragment extends Fragment {
 
-	private static final String ARG_POSITION = "position";
+    private static final String ARG_POSITION = "position";
 
     @InjectView(R.id.textView)
     TextView textView;
 
-	private int position;
+    private int position;
 
-	public static SuperAwesomeCardFragment newInstance(int position) {
-		SuperAwesomeCardFragment f = new SuperAwesomeCardFragment();
-		Bundle b = new Bundle();
-		b.putInt(ARG_POSITION, position);
-		f.setArguments(b);
-		return f;
-	}
+    public static SuperAwesomeCardFragment newInstance(int position) {
+        SuperAwesomeCardFragment f = new SuperAwesomeCardFragment();
+        Bundle b = new Bundle();
+        b.putInt(ARG_POSITION, position);
+        f.setArguments(b);
+        return f;
+    }
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		position = getArguments().getInt(ARG_POSITION);
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        position = getArguments().getInt(ARG_POSITION);
+    }
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_card,container,false);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_card, container, false);
         ButterKnife.inject(this, rootView);
-        ViewCompat.setElevation(rootView,50);
-        textView.setText("CARD "+position);
-		return rootView;
-	}
+        ViewCompat.setElevation(rootView, 50);
+        textView.setText("CARD " + position);
+        return rootView;
+    }
 }
