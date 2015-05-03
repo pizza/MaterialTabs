8914aeaff61e513aaf50b530ffd5ddbfac98ff54
Karim Frenn <karim@oneplus.net>
2015-05-03 19:30:16 +0300

    Deleted menu_share and removed the share button from TabsActivity.

--- sample/src/io/karim/materialtabs/sample/TabsActivity.java
+++ sample/src/io/karim/materialtabs/sample/TabsActivity.java
@@ -29,8 +29,6 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.TypedValue;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -70,32 +68,6 @@ public class TabsActivity extends AppCompatActivity {
 
         final int pageMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4, getResources().getDisplayMetrics());
         mViewPager.setPageMargin(pageMargin);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_tabs, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        switch (item.getItemId()) {
-            case R.id.action_share:
-                Intent sendIntent = new Intent();
-                sendIntent.setAction(Intent.ACTION_SEND);
-                // Replace text with generated code ;-)
-                sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
-                sendIntent.setType("text/plain");
-                startActivity(sendIntent);
-
-                return true;
-
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     private void applyParametersFromIntentExtras() {
